26b4ffa87e585664c6633c359d96957a15917b43
Dominik Teiml <18601956+hacker-DOM@users.noreply.github.com>
2022-06-08 18:18:27 +0200

    :fire: Rm emoji fonts

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 OUT_FILE="abch-${PWD##*/}.pdf";
 python3 -m make_report && \
 asciidoctor-pdf \
-    --require="./lib/emoji-inline-macro.rb" \
-    -a allow-uri-read \
     --verbose \
     --trace \
     --failure-level=ERROR \
