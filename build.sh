6182fd8abecb79323e093e99b42967f4bdf2aecc
Dominik Teiml <18601956+hacker-DOM@users.noreply.github.com>
2022-06-08 18:15:54 +0200

    :card_index_dividers: Backup

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 OUT_FILE="abch-${PWD##*/}.pdf";
 python3 -m make_report && \
 asciidoctor-pdf \
+    --require="./lib/emoji-inline-macro.rb" \
+    -a allow-uri-read \
     --verbose \
     --trace \
     --failure-level=ERROR \
